fd48dcb0bd34e5a107885c73c973e5981481f463
Ozgun Emre Zor <ozgun.zor@veripark.com>
2017-04-13 15:31:46 +0300

    Added offline database working

--- Grocr/AppDelegate.swift
+++ Grocr/AppDelegate.swift
@@ -27,9 +27,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
   
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]? = [:]) -> Bool {
+  private func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]? = [:]) -> Bool {
     UIApplication.shared.statusBarStyle = .lightContent
     FIRApp.configure()
+    FIRDatabase.database().persistenceEnabled = true
     return true
   }
 
